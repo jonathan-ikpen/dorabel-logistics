e86870ef4c916d5dca4883ec20d3d512be74260a
Jonathan Ikpen <jonathanikpen4@gmail.com>
2026-01-13 20:57:50 +0100

    fix: removed allowedHosts in vite config

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,5 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    allowedHosts: [
-      "localhost",
-      "127.0.0.1",
-      "0.0.0.0",
-      "::1",
-      "*",
-      "a557ed41932c.ngrok-free.app",
-    ],
   },
 });
