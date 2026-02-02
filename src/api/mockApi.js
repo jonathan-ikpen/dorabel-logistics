8ad9f15a098b28003bfe96e411726a7e021caa14
Jonathan Ikpen <jonathanikpen4@gmail.com>
2026-02-02 22:49:33 +0100

    Update access key for contact form type

--- src/api/mockApi.js
+++ src/api/mockApi.js
@@ -55,7 +55,7 @@ export async function submitContact(payload) {
 
   const formData = new FormData();
   if (payload.form_type === "contact") {
-    formData.append("access_key", "Зес62242-с8a8-4390-a9a0-561096c456b0");
+    formData.append("access_key", "3ec62242-c8a8-4390-a9a0-561096c456b0");
   }
   if (payload.form_type === "booking") {
     formData.append("access_key", "de028481-b57a-4176-9415-d9b5ba7e63a1");
