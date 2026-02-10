0400838b8c6c858e77131924ebe4debdfe5fffed
codejagaban <okpusjamin@gmail.com>
2026-02-10 20:33:31 +0000

    chore: remove seo todos

--- tina/__generated__/client.d.ts
+++ tina/__generated__/client.d.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'fa45e93400fb5a236b5b125c53a06b8d743426c4', queries,  });
+export const client = createClient({ cacheDir: '/Users/codejagaban/Developer/dorabel-logistics/tina/__generated__/.cache/1770743431420', url: 'https://content.tinajs.io/2.0/content/a0f40b6f-226e-4ecc-9d86-8699247d9668/github/main', token: 'fa45e93400fb5a236b5b125c53a06b8d743426c4', queries,  });
 export default client;
   
